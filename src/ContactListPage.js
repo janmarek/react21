dbc5c6a06fb47419991975ff64587576f0eab187
Jan Marek <mail@janmarek.net>
2021-11-24 15:35:06 +0100

    smart & dumb components

--- src/ContactListPage.js
+++ src/ContactListPage.js
@@ -4,12 +4,19 @@ import { Link } from "react-router-dom";
 import { ROUTE_ADD } from "./routes";
 import { useContacts } from "./contactsModel";
 import { Alert } from "react-bootstrap";
+import styled from "styled-components";
 
+// smart component
 export function ContactListPage() {
   const { isError, isLoading, data: contacts } = useContacts();
 
+  return <ContactListPageView {...{ isError, isLoading, contacts }} />;
+}
+
+// dumb component
+export function ContactListPageView({ isError, isLoading, contacts }) {
   return (
-    <>
+    <ContactListPageStyle>
       <h1>Contact List</h1>
       {isError && <Alert variant="danger">Error</Alert>}
       <p>
@@ -17,9 +24,16 @@ export function ContactListPage() {
       </p>
       {isLoading && <p>Loading...</p>}
       {contacts && <ContactList contacts={contacts} />}
-    </>
+    </ContactListPageStyle>
   );
 }
+
+// styled component
+const ContactListPageStyle = styled.div`
+  h1 {
+    color: red;
+  }
+`;
 
 function ContactList({ contacts }) {
   return (
